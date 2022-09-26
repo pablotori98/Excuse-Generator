3ea98e9164a5aee3a429121924197cd6e0e4e09b
pablotori98 <pablotori98@gmail.com>
2022-09-26 18:34:26 +0000

    Excuse Generator version 1

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse;
+  let whoMath = who[Math.floor(Math.random() * 4)];
+  let whatMath = what[Math.floor(Math.random() * 4)];
+  let whenMath = when[Math.floor(Math.random() * 4)];
+  excuse = whoMath + " " + whatMath + " " + whenMath;
+  document.querySelector("#excuse").innerHTML = excuse;
 };
